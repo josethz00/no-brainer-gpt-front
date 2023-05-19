6b3b2dc339fcdfa99c2d1152ef9d836ed86715f3
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-19 17:36:01 -0300

    styles: input file

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { FormEvent, ChangeEvent, useState } from 'react';
+import Header from '../components/Header';
 
 interface UploadedFile {
   name: string;
@@ -31,26 +32,29 @@ const Upload = () => {
   };
 
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center">
-      <form onSubmit={handleSubmit} className="max-w-xl w-full m-4 p-6 bg-white/20 shadow-md rounded-md">
-        <div className="flex items-center h-12 bg-white/40 rounded-lg mb-4">
-          <input type="file" accept="image/*" onChange={handleFileChange} className="flex-grow px-4 py-2 text-gray-700 bg-transparent outline-none" />
+    <main>
+        <Header />
+        <div className="w-full h-screen flex flex-col items-center justify-center">
+            <form onSubmit={handleSubmit} className="max-w-xl w-full m-4 p-6 bg-white/20 shadow-md rounded-md">
+                <div className="flex items-center h-12 bg-white/40 rounded-lg mb-4">
+                <input type="file" accept=".md, .txt" onChange={handleFileChange} className="flex-grow px-4 py-2 text-gray-700 bg-transparent outline-none" />
+                </div>
+                <button type="submit" className="w-full text-white px-4 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md">
+                    Upload
+                </button>
+            </form>
+            <div className="max-w-xl w-full m-4 p-6 bg-white/20 shadow-md rounded-md overflow-y-auto max-h-64">
+                <h2 className="text-xl mb-4">Uploaded Files:</h2>
+                {uploadedFiles.length ? uploadedFiles.map((file, index) => (
+                <div key={index} className="border-b border-gray-200 py-2">
+                    <p><strong>Name:</strong> {file.name}</p>
+                    <p><strong>Size:</strong> {file.size} bytes</p>
+                    <p><strong>Type:</strong> {file.type}</p>
+                </div>
+                )) : <p>No files uploaded yet.</p>}
+            </div>
         </div>
-        <button type="submit" className="w-full text-white px-4 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md">
-          Upload
-        </button>
-      </form>
-      <div className="max-w-xl w-full m-4 p-6 bg-white/20 shadow-md rounded-md overflow-y-auto max-h-64">
-        <h2 className="text-xl mb-4">Uploaded Files:</h2>
-        {uploadedFiles.length ? uploadedFiles.map((file, index) => (
-          <div key={index} className="border-b border-gray-200 py-2">
-            <p><strong>Name:</strong> {file.name}</p>
-            <p><strong>Size:</strong> {file.size} bytes</p>
-            <p><strong>Type:</strong> {file.type}</p>
-          </div>
-        )) : <p>No files uploaded yet.</p>}
-      </div>
-    </div>
+    </main>
   );
 };
 
