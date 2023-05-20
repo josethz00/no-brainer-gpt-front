4e185b8aab2c229c39a8858dfc8fbabbd98c5ac6
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-19 22:58:22 -0300

    lint(upload): fix indentation

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FormEvent, ChangeEvent, useState } from 'react';
+import { FormEvent, ChangeEvent, useState, useRef, useEffect } from 'react';
 import Header from '../components/Header';
 import { ToastContainer, toast } from 'react-toastify';
 
@@ -12,6 +12,7 @@ interface UploadedFile {
 const Upload = () => {
   const [files, setFiles] = useState<File[]>([]);
   const [uploadedFiles, setUploadedFiles] = useState<UploadedFile[]>([]);
+  const isFirstRender = useRef(true);
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const fileList = e.target.files;
@@ -23,62 +24,69 @@ const Upload = () => {
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     if (Array.isArray(files) && files.length > 0) {
-        const data = new FormData()
+      const data = new FormData()
 
-        files.forEach((file) => {
-            data.append('md_files', file)
+      files.forEach((file) => {
+        data.append('md_files', file)
+      });
+
+      try {
+        console.log('Uploading file...')
+        const result = await fetch('http://localhost:5001/qa/upload-files/form', {
+          method: 'POST',
+          body: data,
         });
-
-        try {
-            console.log('Uploading file...')
-            const result = await fetch('http://localhost:5001/qa/upload-files/form', {
-                method: 'POST',
-                body: data,
-            });
-            if (result.status === 202) {
-                setUploadedFiles([
-                    ...uploadedFiles,
-                    ...files.map((file) => ({
-                        name: file.name,
-                        size: file.size,
-                        type: file.type,
-                    })),
-                ]);
-                setFiles([]);
-                toast('Files received, processing...', { type: 'success' })
-            }
-        } catch(err) {
-            console.error(err);
-            return
+        if (result.status === 202) {
+          setUploadedFiles([
+            ...uploadedFiles,
+            ...files.map((file) => ({
+              name: file.name,
+              size: file.size,
+              type: file.type,
+            })),
+          ]);
+          setFiles([]);
+          toast('Files received, processing...', { type: 'success' })
         }
+      } catch(err) {
+          console.error(err);
+          return
+      }
     }
   };
 
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+  }, [uploadedFiles]);
+
   return (
     <main>
-        <ToastContainer />
-        <Header />
-        <div className="w-full h-screen flex flex-col items-center justify-center">
-            <form onSubmit={handleSubmit} className="max-w-xl w-full m-4 p-6 bg-white/20 shadow-md rounded-md">
-                <div className="flex items-center h-12 bg-white/40 rounded-lg mb-4">
-                <input type="file" accept=".md, .txt" onChange={handleFileChange} className="flex-grow px-4 py-2 text-gray-700 bg-transparent outline-none" />
-                </div>
-                <button type="submit" className="w-full text-white px-4 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md">
-                    Upload
-                </button>
-            </form>
-            <div className="max-w-xl w-full m-4 p-6 bg-white/20 shadow-md rounded-md overflow-y-auto max-h-64">
-                <h2 className="text-xl mb-4">Uploaded Files:</h2>
-                {uploadedFiles.length ? uploadedFiles.map((file, index) => (
-                    <div key={index} className="border-b border-gray-200 py-2">
-                        <p><strong>Name:</strong> {file.name}</p>
-                        <p><strong>Size:</strong> {file.size} bytes</p>
-                        <p><strong>Type:</strong> {file.type}</p>
-                    </div>
-                    )) : <p>No files uploaded yet.</p>
-                }
-            </div>
+      <ToastContainer />
+      <Header />
+      <div className="w-full h-screen flex flex-col items-center justify-center">
+        <form onSubmit={handleSubmit} className="max-w-xl w-full m-4 p-6 bg-white/20 shadow-md rounded-md">
+          <div className="flex items-center h-12 bg-white/40 rounded-lg mb-4">
+            <input type="file" accept=".md, .txt" onChange={handleFileChange} className="flex-grow px-4 py-2 text-gray-700 bg-transparent outline-none" />
+          </div>
+          <button type="submit" className="w-full text-white px-4 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md">
+            Upload
+          </button>
+        </form>
+        <div className="max-w-xl w-full m-4 p-6 bg-white/20 shadow-md rounded-md overflow-y-auto max-h-64">
+          <h2 className="text-xl mb-4">Uploaded Files:</h2>
+            {uploadedFiles.length ? uploadedFiles.map((file, index) => (
+              <div key={index} className="border-b border-gray-200 py-2">
+                <p><strong>Name:</strong> {file.name}</p>
+                <p><strong>Size:</strong> {file.size} bytes</p>
+                <p><strong>Type:</strong> {file.type}</p>
+              </div>
+              )) : <p>No files uploaded yet.</p>
+            }
         </div>
+      </div>
     </main>
   );
 };
