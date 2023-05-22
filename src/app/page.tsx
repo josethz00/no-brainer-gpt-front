ae8daf75f109e72d34346eda3158d3e19145ed8e
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-22 17:47:30 -0300

    feat: add loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,16 @@ function Home() {
     <main>
       <Header />
       <SearchBar onSearch={handleSearch} />
-      <ResultBox results={results} />
+      {isLoading ? (
+        <div className="flex justify-center items-center h-96">
+          <svg className="animate-spin h-10 w-10 text-white" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="3"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8"></path>
+          </svg>
+        </div>
+      ) : (
+        <ResultBox results={results} />
+      )}
     </main>
   );
 }
